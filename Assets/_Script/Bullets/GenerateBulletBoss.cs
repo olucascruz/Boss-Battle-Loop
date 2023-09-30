70f829bd088a49141384e8dc05a670afc1565144
LucasCruz <lca.snf21@uea.edu.br>
2023-09-30 16:10:29 -0400

    feat: add speed parametrer and sound

--- Assets/_Script/Bullets/GenerateBulletBoss.cs
+++ Assets/_Script/Bullets/GenerateBulletBoss.cs
@@ -5,23 +5,38 @@ using UnityEngine;
 public class GenerateBulletBoss : MonoBehaviour
 {
     [SerializeField] private GameObject Bullet;
-    public void Shoot(Vector3? direction = null, Transform targetBullet = null)
+    private SoundManager soundManager;
+    [SerializeField] private AudioSource bossBulletSound;
+
+    void Start()
+    {
+        soundManager = SoundManager.Instance;
+    }
+
+    public void Shoot(Vector3? direction = null, Transform targetBullet = null, float speed = 0f)
     {
 
         GameObject bullet = Instantiate(Bullet, transform.position, transform.rotation);
         Vector3 velocity = Vector3.zero;
+
+        if(soundManager && bossBulletSound){
+            soundManager.AttackBossSoundPlay(bossBulletSound);
+        }else if(soundManager==null){
+            print("Sound manager not found");
+        }
+
         float bulletSpeed = bullet.GetComponent<Bullet>().Speed;
+        if(speed != 0f){
+            bulletSpeed = speed;
+        }
 
         if(direction != null){
             velocity = (Vector3) direction * bulletSpeed;
-            print($"velocity with direction: {velocity}");
-
         }
 
         if(targetBullet != null){
             Vector3 targetDirection = (targetBullet.position - transform.position).normalized;
             velocity = targetDirection * bulletSpeed * 1.2f;
-            print($"velocity with target: {velocity}");
 
         }
 
